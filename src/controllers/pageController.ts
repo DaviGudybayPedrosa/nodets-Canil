0a62afed05c56b7a39f9dc01a1e1ec7663a31c77
davi pedrosa <davigudybaypedrosa@gmail.com>
2022-05-20 12:00:08 -0300

    TESTE

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,48 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
+
 
 export const home = (req: Request, res: Response) => {
-    res.send('home no controller!');
-    //res.render('pages/page');
+    
+    res.render('pages/page',{
+        menu: createMenuObject('all'),
+        banner:{
+            tittle: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+    });
 }
 
 export const dogs = (req: Request, res: Response) => {
 
-    //res.render('pages/page');
+    res.render('pages/page',{
+        menu: createMenuObject('dog'),
+        banner:{
+            tittle: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
+
 }
 
 export const cats = (req: Request, res: Response) => {
 
-    //res.render('pages/page');
+    res.render('pages/page',{
+        menu: createMenuObject('cat'),
+        banner:{
+            tittle: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 
 export const fishes = (req: Request, res: Response) => {
 
-    //res.render('pages/page');
+    res.render('pages/page',{
+        menu: createMenuObject('fish'), 
+        banner:{
+            tittle: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+    });
 }
---
+++ src/models/pet.ts
@@ -0,0 +1,60 @@
+type PetType = 'dog' | 'cat' | 'fish';
+type PetSex = 'Masculino' | 'Feminino';
+type Pet = {
+    type: PetType,
+    image: string,
+    name: string,
+    color: string,
+    sex: PetSex
+};
+
+const data: Pet[] = [
+    {
+        type: 'dog',
+        image: 'pastor-alemao.jpg',
+        name: 'Pastor-alemão',
+        color: 'Amarelo e Preto',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'labrador.jpg',
+        name: 'labrador-retriver',
+        color: 'Branco',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'zwergspitz.jpg',
+        name: 'Zwergspitz',
+        color: 'Amarelo',
+        sex: 'Feminino'
+    },
+    {
+        type: 'dog',
+        image: 'husky.jpg',
+        name: 'Husky Siberiano',
+        color: 'Branco e Preto',
+        sex: 'Masculino'
+    },
+
+];
+
+export const Pet = {
+    getAll: (): Pet[] => {
+        return data;
+    },
+
+    getFromType: (type: PetType): Pet[] => {
+        return data.filter(item => item.type === type);
+    },
+
+    getFromName:(name: string): Pet[] => {
+        return data.filter(item => item.name.toLowerCase().indexOf(name.toLowerCase()) > -1);
+    }
+
+};
+
+
+
+
