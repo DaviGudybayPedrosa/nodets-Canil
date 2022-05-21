dda409e061d77e47c0318be2ea313dc647f8874e
davi pedrosa <davigudybaypedrosa@gmail.com>
2022-05-21 19:23:47 -0300

    Final

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,26 +1,31 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet'
 import { createMenuObject } from '../helpers/createMenuObject';
 
 
 export const home = (req: Request, res: Response) => {
     
+    let list = Pet.getAll();
     res.render('pages/page',{
         menu: createMenuObject('all'),
         banner:{
             tittle: 'Todos os animais',
             background: 'allanimals.jpg'
         },
+        list
     });
 }
 
 export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog');
 
     res.render('pages/page',{
         menu: createMenuObject('dog'),
         banner:{
             tittle: 'Cachorros',
             background: 'banner_dog.jpg'
-        }
+        },
+        list
     });
 
 }
